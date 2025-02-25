cfb92477ae00d820af50ea6e2013990a9c1f65ed
Janio Muianga <janiodrey@gmail.com>
2025-02-25 11:38:15 +0200

    add metodo refresh access token

--- R/authentication/refresh_access_token.R
+++ R/authentication/refresh_access_token.R
@@ -1,1 +1,14 @@
-# exemplo
+
+refresh_access_token <- function(client_id, client_secret, refresh_token){
+  # composição da url
+  url <- glue("https://accounts.zoho.com/oauth/v2/token?refresh_token={refresh_token}&client_id={client_id}&client_secret={client_secret}&grant_type=refresh_token")
+
+  # request the access token
+  response_data <- request(url) |>
+    req_method("POST") |>
+    req_perform() |>
+    resp_body_json()
+
+  # returns the access token
+  return(response_data$access_token)
+}

57c49eaf238324f9d2896417f26832f8bf8587e7
nhanombedercio <nhanombe.benaldo@gmail.com>
2025-03-05 15:53:52 +0200

    actualizacao da documentacao da funcao get_records

--- R/data-apis/get_records.R
+++ R/data-apis/get_records.R
@@ -1,4 +1,32 @@
-#' @title Get Records
+#' @title Get Records for Zoho Creator
+#'
+#' @description
+#' Esta função busca registros de um relatório no Zoho Creator usando a API.
+#'
+#' @param account_owner_name Nome do proprietário da conta.
+#' @param app_name Nome do aplicativo dentro do Zoho Creator.
+#' @param report_name Nome do relatório de onde os registros serão extraídos.
+#' @param access_token Token de acesso para autenticação na API do Zoho.
+#'
+#' @return Um data frame contendo os registros extraídos do relatório.
+#'
+#' @details
+#' A função realiza múltiplas requisições à API para recuperar até 1000 registros por vez,
+#' continuando enquanto houver registros adicionais disponíveis. A função só para
+#'  depois de buscar todos os registos do relatorio.
+#'
+#' #' @examples
+#' # Exemplo de uso da função com autenticação via arquivo .env
+#' library(dotenv)
+#' load_dot_env(".env")
+#'
+#' account_owner <- Sys.getenv("ZOHO_ACCOUNT_OWNER")
+#' app <- Sys.getenv("ZOHO_APP_NAME")
+#' report <- Sys.getenv("ZOHO_REPORT_NAME")
+#' token <- Sys.getenv("ZOHO_ACCESS_TOKEN")
+#'
+#' registros <- get_records(account_owner, app, report, token)
+#' view(registros)
 #'
 #' @export
 get_records <- function(account_owner_name, app_name, report_name, access_token){
@@ -9,6 +37,7 @@ get_records <- function(account_owner_name, app_name, report_name, access_token)
 
   # URL da API
   url <- glue("https://zohoapis.com/creator/v2.1/data/{account_owner_name}/{app_name}/report/{report_name}")
+
 
   while (more_records) {
     # requisição para buscar os dados
@@ -43,6 +72,7 @@ get_records <- function(account_owner_name, app_name, report_name, access_token)
     } else {
       stop("Erro na requisição: ", response_data$status_code)
     }
+
   }
 
   # Converter lista de dados em um data frame final
